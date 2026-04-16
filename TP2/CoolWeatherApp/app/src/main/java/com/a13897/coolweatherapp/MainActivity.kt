ce918889d3c686ea8c7d0420b03c509d9a5f8886
David13897 <13897@enautica.pt>
2026-04-17 00:30:32 +0100

    front end

--- TP2/CoolWeatherApp/app/src/main/java/com/a13897/coolweatherapp/MainActivity.kt
+++ TP2/CoolWeatherApp/app/src/main/java/com/a13897/coolweatherapp/MainActivity.kt
@@ -8,13 +8,26 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val day = true
+
+        when (resources.configuration.orientation) {
+            android.content.res.Configuration.ORIENTATION_PORTRAIT -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day)
+                } else {
+                    setTheme(R.style.Theme_Night)
+                }
+            }
+            android.content.res.Configuration.ORIENTATION_LANDSCAPE -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day_Land)
+                } else {
+                    setTheme(R.style.Theme_Night_Land)
+                }
+            }
         }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
